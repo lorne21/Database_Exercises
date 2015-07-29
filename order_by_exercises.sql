30d1cc88498665af867d1a18eb089199ac93c109
Lorne Barfield <lornebarfield1@Lornes-MacBook-Pro.local>
2015-07-29 13:05:43 -0500

    order by exercises

---
+++ order_by_exercises.sql
@@ -0,0 +1,36 @@
+SELECT first_name, last_name
+FROM employees
+WHERE first_name ='Irena'
+AND gender = 'M'
+OR first_name = 'Vidya'
+AND gender = 'M'
+OR first_name = 'Maya'
+AND gender = 'M'
+ORDER BY first_name, last_name;
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+OR last_name LIKE '%e'
+ORDER BY emp_no DESC;
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%e'
+ORDER BY emp_no DESC;
+
+SELECT first_name, last_name 
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+AND birth_date LIKE '%-12-25'
+ORDER BY hire_date DESC, birth_date DESC;
+
+SELECT first_name
+FROM employees
+WHERE birth_date LIKE '%-12-25';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name LIKE '%qu%'; 
